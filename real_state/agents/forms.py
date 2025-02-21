11ebf0c356b5efa0ca0aceabeade6afcc7f7719e
Samuel Henao <codinsamueldev@gmail.com>
2025-02-21 15:17:53 -0500

    fix: forms for users and realtors not storing data
    The problem: save user obj before creation

--- real_state/agents/forms.py
+++ real_state/agents/forms.py
@@ -28,28 +28,43 @@ class UserRegistrationForm(UserCreationForm):
     
     @transaction.atomic
     def save(self, commit=True):
+        """ 
+        The decorator is used to perform a consistent data storage.
+        This way if something goes wrong in the middle the user will not be created.
+        Enhancing data consistency thorough all DB.
+        """
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
-        
+        user.password = self.cleaned_data['password']
+        user.phone_number = self.cleaned_data['phone_number']
+        user.address = self.cleaned_data['address']
+
         if commit:
             user.save()
-            user_profile = UserProfile.objects.get(user=user)
-            user_profile.phone_number = self.cleaned_data['phone_number']
-            user_profile.address = self.cleaned_data['address']
+            user_profile = UserProfile.objects.create(user=user)
             user_profile.save()
             
         return user
 
-class RealtorRegistrationForm(UserRegistrationForm):
+class RealtorRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone_number = SplitPhoneNumberField()
+    address = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), required=True)
     agency_name = forms.CharField(max_length=100, required=True)
     license_number = forms.CharField(max_length=50, required=True)
     years_of_experience = forms.IntegerField(min_value=0, required=True)
     
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with this email already exists.")
+        return email
+    
     def clean_license_number(self):
-        """
-        Make sure each Realtor has a unique license number in the platform.
-        This is done to ensure an extra layer of security for home-buyers in decision making.
-        """
         license_number = self.cleaned_data.get('license_number')
         if RealtorProfile.objects.filter(license_number=license_number).exists():
             raise ValidationError("A realtor with this license number already exists.")
@@ -57,25 +72,25 @@ class RealtorRegistrationForm(UserRegistrationForm):
     
     @transaction.atomic
     def save(self, commit=True):
-        """ 
-        The decorator is used to perform a consistent data storage.
-        This way if something goes wrong in the middle the user will not be created.
-        Enhancing data consistency thorough all DB.
-        """
         user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
         
         if commit:
             user.save()
-            user_profile = UserProfile.objects.get(user=user)
-            user_profile.is_realtor = True
-            user_profile.save()
+            # Create UserProfile first
+            user_profile = UserProfile.objects.create(
+                user=user,
+                phone_number=self.cleaned_data['phone_number'],
+                address=self.cleaned_data['address'],
+                is_realtor=True
+            )
             
+            # Now create RealtorProfile
             realtor_profile = RealtorProfile.objects.create(
                 user_profile=user_profile,
                 agency_name=self.cleaned_data['agency_name'],
                 license_number=self.cleaned_data['license_number'],
                 years_of_experience=self.cleaned_data['years_of_experience']
             )
-            
+        
         return user
-
