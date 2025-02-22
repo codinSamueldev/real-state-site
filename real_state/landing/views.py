8e20e1ffa39e11b26de76a264532760916379bd4
Samuel Henao <codinsamueldev@gmail.com>
2025-02-22 11:46:18 -0500

    update landing featured properties section by renderization of properties obj

--- real_state/landing/views.py
+++ real_state/landing/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+from .models import Properties
+
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html", {})
+    return render(request, "index.html", {'property': Properties.objects.all()})
 
