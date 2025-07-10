189e06352720577fb95e6da8a6be7b360e9137a4
Samuel Henao <codinsamueldev@gmail.com>
2025-07-10 12:38:42 -0500

    add security settings, install whitenoise and update src dir for json builder file

--- real_state/real_state/settings.py
+++ real_state/real_state/settings.py
@@ -34,6 +34,14 @@ ALLOWED_HOSTS = ['realstatedeploy77.pythonanywhere.com', '.vercel.app']
 
 SECURE_SSL_REDIRECT = True
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_HSTS_SECONDS = 31536000
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
+
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
@@ -148,9 +156,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-if DEBUG: STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else: STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+# For Vercel deployment
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # User-uploaded files.
