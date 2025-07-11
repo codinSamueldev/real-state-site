856e208cd26e499540bb2a20812759b25ca17245
Samuel Henao <codinsamueldev@gmail.com>
2025-07-10 20:44:15 -0500

    specify python version

--- real_state/build_files.sh
+++ real_state/build_files.sh
@@ -7,4 +7,4 @@ echo "Building the project..."
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput
